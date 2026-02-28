3a701de6c9b9da665fc61c8c8ae27a2b2db5c91f
MakMouse <maksfrankov2008@gmail.com>
2026-02-28 22:36:27 +0200

    Added adaptive Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import Header from './Components/Header/Header.jsx';
 
-function App() {
-  
+const App = () => {
+
   return (
-    <>
-    </>
-  )
+    <div className="app-container">
+      <Header />
+    </div>
+  );
 }
 
 export default App
